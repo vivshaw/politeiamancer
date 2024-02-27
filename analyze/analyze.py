c2dbc8073a54de9fdc403fec634c08a6a6c22824
vivshaw <hey@vivsha.ws>
2024-02-27 01:20:41 -0500

    actually use a linter/formatter, haha

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -10,8 +10,21 @@ Analysis of Social Media Text. Eighth International Conference on Weblogs and So
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, current_timestamp, from_json, from_unixtime, lower, udf
-from pyspark.sql.types import FloatType, IntegerType, StringType, StructField, StructType  
+from pyspark.sql.functions import (
+    col,
+    current_timestamp,
+    from_json,
+    from_unixtime,
+    lower,
+    udf,
+)
+from pyspark.sql.types import (
+    FloatType,
+    IntegerType,
+    StringType,
+    StructField,
+    StructType,
+)
 
 # Cassandra and Kafka connection params
 CASSANDRA_COMMENTS_TABLE = "r_politics_comments"
@@ -22,97 +35,104 @@ KAFKA_BOOTSTRAP_SERVERS = "kafka1:9092"
 KAFKA_TOPIC = "r_politics_comments"
 
 # Schema of my Kafka data
-kafka_schema = StructType([
-    # ID
-    StructField("fullname", StringType(), True),
+kafka_schema = StructType(
+    [
+        # ID
+        StructField("fullname", StringType(), True),
+        # Comment details
+        StructField("author", StringType(), True),
+        StructField("body", StringType(), True),
+        StructField("downvotes", IntegerType(), True),
+        StructField("permalink", StringType(), True),
+        StructField("upvotes", IntegerType(), True),
+        # Time
+        StructField("timestamp", FloatType(), True),
+    ]
+)
 
-    # Comment details
-    StructField("author", StringType(), True),
-    StructField("body", StringType(), True),
-    StructField("downvotes", IntegerType(), True),
-    StructField("permalink", StringType(), True),
-    StructField("upvotes", IntegerType(), True),
-
-    # Time
-    StructField("timestamp", FloatType(), True),
-])
 
 @udf(returnType=FloatType())
 def sentiment_score(text: str) -> float:
     """
     Spark UDF to calculate a sentiment score for a piece of text using VADER.
-    (Can't be defined inside `RedditCommentAnalyzer` or else Spark complains, so it lives out here.)
+    (Can't be defined inside `RedditCommentAnalyzer` or else Spark complains, so it
+    lives out here.)
     """
-    sentiment_intensity_analyzer = SentimentIntensityAnalyzer()  
+    sentiment_intensity_analyzer = SentimentIntensityAnalyzer()
     valence_scores = sentiment_intensity_analyzer.polarity_scores(text)
-    sentiment = valence_scores['compound']
+    sentiment = valence_scores["compound"]
     return sentiment
+
 
 class RedditCommentAnalyzer:
     """
-    A class that stands up a Spark instance and uses it to run streaming sentiment analysis
-    on Reddit comment data. Data is pulled from Kafka and streamed into Cassandra.
+    A class that stands up a Spark instance and uses it to run streaming sentiment
+    analysis on Reddit comment data. Data is pulled from Kafka and streamed into
+    Cassandra.
     """
+
     def __init__(self) -> None:
-        self.spark: SparkSession = SparkSession.builder \
-            .appName("StreamProcessor") \
-            .config('spark.cassandra.connection.host', CASSANDRA_HOST) \
-            .config('spark.cassandra.connection.port', CASSANDRA_PORT) \
-            .config('spark.cassandra.output.consistency.level','ONE') \
+        self.spark: SparkSession = (
+            SparkSession.builder.appName("StreamProcessor")
+            .config("spark.cassandra.connection.host", CASSANDRA_HOST)
+            .config("spark.cassandra.connection.port", CASSANDRA_PORT)
+            .config("spark.cassandra.output.consistency.level", "ONE")
             .getOrCreate()
+        )
 
     def stream_comment_analysis(self) -> None:
         """
-        Performs streaming data analysis on a live stream of /r/politics comments pulled from Kafka.
+        Performs streaming data analysis on a live stream of /r/politics comments
+        pulled from Kafka.
 
         Specifically:
          - calculates a sentiment score for each comment, using VADER
          - checks whether Biden or Trump is mentioned in each comment
         """
         # First, ingest the raw data from Kafka
-        raw_df = self.spark \
-            .readStream \
-            .format("kafka") \
-            .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
-            .option("subscribe", KAFKA_TOPIC) \
+        raw_df = (
+            self.spark.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
+            .option("subscribe", KAFKA_TOPIC)
             .load()
-        
+        )
+
         # Parse out the JSON
-        parsed_df = raw_df \
-            .withColumn(
-                "comment_json",
-                from_json(col("value").cast("string"), kafka_schema) # Needs an explicit cast 'cause Spark infers type `BINARY`
-            ) \
-            .select(
-                "comment_json.*"
-            )
+        parsed_df = raw_df.withColumn(
+            "comment_json",
+            from_json(
+                col("value").cast("string"), kafka_schema
+            ),  # Needs an explicit cast 'cause Spark infers type `BINARY`
+        ).select("comment_json.*")
 
         # Munge the timestamps a bit.
-        # We want a separate timestamp for the comment's timestamp, and the time we ran the analysis.
-        with_timestamps_df = parsed_df \
-            .withColumn("comment_timestamp", from_unixtime(col("timestamp"))) \
-            .withColumn("analysis_timestamp", current_timestamp()) \
+        # We want a separate timestamp for the comment's timestamp, and the time we
+        # ran the analysis.
+        with_timestamps_df = (
+            parsed_df.withColumn("comment_timestamp", from_unixtime(col("timestamp")))
+            .withColumn("analysis_timestamp", current_timestamp())
             .drop("timestamp")
-        
+        )
+
         # Check whether Biden or Trump were mentioned
-        with_mentions_df = with_timestamps_df \
-            .withColumn("mentions_trump", lower(col("body")).like("%trump%")) \
-            .withColumn("mentions_biden", lower(col("body")).like("%biden%"))
-        
+        with_mentions_df = with_timestamps_df.withColumn(
+            "mentions_trump", lower(col("body")).like("%trump%")
+        ).withColumn("mentions_biden", lower(col("body")).like("%biden%"))
+
         # Calculate a sentiment score
-        with_sentiments_df = with_mentions_df.withColumn('sentiment_score', sentiment_score(col("body")))
+        with_sentiments_df = with_mentions_df.withColumn(
+            "sentiment_score", sentiment_score(col("body"))
+        )
 
         # Write out to Cassandra
-        with_sentiments_df \
-            .writeStream \
-            .format("org.apache.spark.sql.cassandra") \
-            .option("checkpointLocation", "/tmp/checkpoint/") \
-            .option("failOnDataLoss", "false") \
-            .option("keyspace", CASSANDRA_KEYSPACE) \
-            .option("table", CASSANDRA_COMMENTS_TABLE) \
-            .start()
-        
+        with_sentiments_df.writeStream.format("org.apache.spark.sql.cassandra").option(
+            "checkpointLocation", "/tmp/checkpoint/"
+        ).option("failOnDataLoss", "false").option(
+            "keyspace", CASSANDRA_KEYSPACE
+        ).option("table", CASSANDRA_COMMENTS_TABLE).start()
+
         self.spark.streams.awaitAnyTermination()
+
 
 if __name__ == "__main__":
     reddit_comment_analyzer = RedditCommentAnalyzer()
--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,27 +1,34 @@
 """Ingests comments from the /r/politics subreddit, and slaps 'em into Kafka."""
 
-import asyncpraw
 import asyncio
 import os
-
 from json import dumps
+
+import asyncpraw
 from kafka import KafkaProducer
 
-# "I already am eating from the trash can all the time. The name of this trash can is ideology."
+# Reddit OAuth creds
+REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
+# "I already am eating from the trash can all the time.
+# The name of this trash can is ideology."
 SUBREDDIT = "politics"
 # I want to reference the app version in the `user_agent`
 VERSION = os.getenv("VERSION")
 
+
 class RedditCommentIngester:
     """
-    A class that stands up a Reddit and Kafka client, and uses them to stream comment data into Kafka.
+    A class that stands up a Reddit and Kafka client, and uses them to stream comment
+    data into Kafka.
     """
+
     def __init__(self) -> None:
         self.reddit = self.__initialize_reddit_client__()
         self.kafka_producer = KafkaProducer(
             api_version=(0, 10, 1),
-            bootstrap_servers = ['kafka1:9092'],
-            value_serializer = lambda x: dumps(x).encode('utf-8')
+            bootstrap_servers=["kafka1:9092"],
+            value_serializer=lambda x: dumps(x).encode("utf-8"),
         )
 
     def __initialize_reddit_client__(self) -> None:
@@ -33,9 +40,9 @@ class RedditCommentIngester:
         Credentials need to be supplied via env var.
         """
         reddit = asyncpraw.Reddit(
-            client_id = os.getenv("REDDIT_CLIENT_ID"),
-            client_secret = os.getenv("REDDIT_CLIENT_SECRET"),
-            user_agent = f"python:vivshaw/politeiamancer:{VERSION} (by /u/vivshaw)",
+            client_id=REDDIT_CLIENT_ID,
+            client_secret=REDDIT_CLIENT_SECRET,
+            user_agent=f"python:vivshaw/politeiamancer:{VERSION} (by /u/vivshaw)",
         )
 
         return reddit
@@ -49,36 +56,38 @@ class RedditCommentIngester:
 
         async for comment in subreddit.stream.comments(skip_existing=False):
             try:
-                # Turn the comment into somethin' JSON-serializable
-                comment_as_json: dict[str, str] = {
+                # Turn the comment into somethin' JSON-serializable, and drop the
+                # fields I don't care about
+                selected_comment_fields: dict[str, str] = {
                     # ID
                     "fullname": comment.name,
-                    
                     # Comment details
                     "author": comment.author.name,
                     "body": comment.body,
                     "downvotes": comment.downs,
                     "permalink": comment.permalink,
                     "upvotes": comment.ups,
-
                     # Time
                     "timestamp": comment.created_utc,
                 }
 
                 # Logs
-                print(f"recv'd: {comment_as_json}")
+                print(f"recv'd: {selected_comment_fields}")
 
                 # Finally, send it to Kafka!
-                self.kafka_producer.send("r_politics_comments", value=comment_as_json)
+                self.kafka_producer.send(
+                    "r_politics_comments", value=selected_comment_fields
+                )
 
             except Exception as e:
                 # If we failed to send to Kafka, let it fail and keep truckin'.
                 print(f"Couldn't send comment to Kafka: {e}")
 
+
 async def main():
     reddit_comment_ingester = RedditCommentIngester()
-    foo = reddit_comment_ingester.stream()
-    await foo
+    await reddit_comment_ingester.stream()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
